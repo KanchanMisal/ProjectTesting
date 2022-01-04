dddfd58e2a5207a823eaa343f78e305d591101f1
KanchanMisal <97090789+KanchanMisal@users.noreply.github.com>
2022-01-04 18:50:52 +0530

    Create UserPrivilegeReqDto.java

---
+++ dto/req/UserPrivilegeReqDto.java
@@ -0,0 +1,17 @@
+package com.task.shopCart.dto.req;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserPrivilegeReqDto {
+    private Integer userPriviId;
+
+    private Boolean uMaster;
+
+    private String uPriviStatus;
+
+
+
+}
