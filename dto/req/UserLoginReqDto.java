d92419e991bf7c245e06503f2405154574b05466
KanchanMisal <97090789+KanchanMisal@users.noreply.github.com>
2022-01-04 18:50:07 +0530

    Create UserLoginReqDto.java

---
+++ dto/req/UserLoginReqDto.java
@@ -0,0 +1,12 @@
+package com.task.shopCart.dto.req;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginReqDto {
+    private String userName;
+
+    private String userPass;
+}
