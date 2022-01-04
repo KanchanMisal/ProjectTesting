cb5d84e41314adfb554b101789d27ec7e134915c
KanchanMisal <97090789+KanchanMisal@users.noreply.github.com>
2022-01-04 18:43:15 +0530

    Create CommonClass.java

---
+++ dto/dtoconfiguration/CommonClass.java
@@ -0,0 +1,12 @@
+package com.task.shopCart.dto.dtoconfiguration;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class CommonClass<T> {
+    private T commonClass;
+}
