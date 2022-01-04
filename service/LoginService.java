1016246674be8491b5df0e3a7df37b502453e504
KanchanMisal <97090789+KanchanMisal@users.noreply.github.com>
2022-01-04 18:56:07 +0530

    Create LoginService.java

---
+++ service/LoginService.java
@@ -0,0 +1,16 @@
+package com.task.shopCart.service;
+
+
+import com.task.shopCart.dto.req.EmpCredentialsReqDto;
+import com.task.shopCart.dto.req.UserLoginReqDto;
+import com.task.shopCart.dto.res.EmpCredentialsResDto;
+import com.task.shopCart.dto.res.UserLoginResDto;
+
+public interface LoginService {
+
+
+
+    UserLoginResDt getUserNameLoginService(UserLoginReqDto userLoginReqDto);
+
+
+}
